e32b22a25b6b0165e4e7a0a09f4364d9db5a78b3
Beregszászi István <beistvan@gmail.com>
2023-09-21 07:35:07 +0200

    Predicting Medical Costs

---
+++ predicting-medical-costs/predicting-medical-costs-loading-data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+#load the dataset
+dataset = pd.read_csv('../data/insurance.csv')
+#choose first 7 columns as features
+features = dataset.iloc[:,0:6]
+#choose the final column for prediction
+labels = dataset.iloc[:,-1]
+
+#print the number of features in the dataset
+print("Number of features: ", features.shape[1])
+#print the number of samples in the dataset
+print("Number of samples: ", features.shape[0])
+#see useful summary statistics for numeric features
+print(features.describe())
+
+# predicting medical costs: shape and describe
+print(labels.shape)
+print(labels.describe())
